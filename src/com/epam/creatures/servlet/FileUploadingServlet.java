f0e2b42962aeab6ea43a1dd5fa13db4b4833e3ec
crazyr <alexey98901@gmail.com>
2018-07-16 11:07:14 +0300

    16.07.2018 11:08

--- src/com/epam/creatures/servlet/FileUploadingServlet.java
+++ src/com/epam/creatures/servlet/FileUploadingServlet.java
@@ -8,6 +8,7 @@ import com.epam.creatures.dao.DAOException;
 import com.epam.creatures.dao.impl.AdminDAO;
 import com.epam.creatures.dao.impl.CreaturesDAO;
 import com.epam.creatures.dao.impl.UserDAO;
+import com.epam.creatures.validator.PictureValidator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -37,45 +38,71 @@ public class FileUploadingServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Part picture = null;
+        StringBuilder errorMessage = new StringBuilder();
 
         try{
             picture = request.getPart(PICTURE);
         }catch (Exception e){
             LOGGER.error(e);
+            errorMessage.append(e).append(".");
         }
 
         if(picture!=null){
             PictureType pictureType = PictureType.valueOf(request.getParameter(ParameterConstant.PICTURE_TYPE_PARAMETER));
+            PictureValidator pictureValidator = new PictureValidator();
             Integer id;
 
             try {
 
                 switch (pictureType) {
                     case CREATURE_IMAGE:
-                        CreaturesDAO creaturesDAO = new CreaturesDAO();
-                        id = Integer.parseInt(request.getParameter(ParameterConstant.CREATURE_ID_PARAMETER));
-                        creaturesDAO.updateCreatureImage(id, picture.getInputStream());
-                        response.sendRedirect(PagePath.ADMIN_MAIN_PAGE);
-                        return;
+
+                        if(pictureValidator.validateCreatureImageSize(picture.getInputStream().readAllBytes())) {
+                            CreaturesDAO creaturesDAO = new CreaturesDAO();
+                            id = Integer.parseInt(request.getParameter(ParameterConstant.CREATURE_ID_PARAMETER));
+                            creaturesDAO.updateCreatureImage(id, picture.getInputStream());
+                            response.sendRedirect(PagePath.ADMIN_MAIN_PAGE);
+                            return;
+                        }
+                        else{
+                            errorMessage.append("Image size is too high.");
+                        }
+                        break;
                     case USER_AVATAR:
-                        UserDAO userDAO = new UserDAO();
-                        id = Integer.parseInt(request.getParameter(ParameterConstant.USER_ID_PARAMETER));
-                        userDAO.updateUserAvatar(id,picture.getInputStream());
-                        request.getSession().setAttribute(AttributeConstant.AVATAR_ATTRIBUTE,Base64.getEncoder().encodeToString(picture.getInputStream().readAllBytes()));
-                        response.sendRedirect(PagePath.USER_MAIN_PAGE);
-                        return;
+
+                        if(pictureValidator.validateAvatarSize(picture.getInputStream().readAllBytes())) {
+                            UserDAO userDAO = new UserDAO();
+                            id = Integer.parseInt(request.getParameter(ParameterConstant.USER_ID_PARAMETER));
+                            userDAO.updateUserAvatar(id, picture.getInputStream());
+                            request.getSession().setAttribute(AttributeConstant.AVATAR_ATTRIBUTE, Base64.getEncoder().encodeToString(picture.getInputStream().readAllBytes()));
+                            response.sendRedirect(PagePath.USER_MAIN_PAGE);
+                            return;
+                        }else{
+                            errorMessage.append("Avatar size is too high.");
+                        }
+                        break;
                     case ADMIN_AVATAR:
-                        AdminDAO adminDAO = new AdminDAO();
-                        id = Integer.parseInt(request.getParameter(ParameterConstant.ADMIN_ID_PARAMETER));
-                        adminDAO.updateAdminAvatar(id,picture.getInputStream());
-                        request.getSession().setAttribute(AttributeConstant.AVATAR_ATTRIBUTE,Base64.getEncoder().encodeToString(picture.getInputStream().readAllBytes()));
-                        response.sendRedirect(PagePath.ADMIN_MAIN_PAGE);
-                        return;
+
+                        if(pictureValidator.validateAvatarSize(picture.getInputStream().readAllBytes())) {
+                            AdminDAO adminDAO = new AdminDAO();
+                            id = Integer.parseInt(request.getParameter(ParameterConstant.ADMIN_ID_PARAMETER));
+                            adminDAO.updateAdminAvatar(id, picture.getInputStream());
+                            request.getSession().setAttribute(AttributeConstant.AVATAR_ATTRIBUTE, Base64.getEncoder().encodeToString(picture.getInputStream().readAllBytes()));
+                            response.sendRedirect(PagePath.ADMIN_MAIN_PAGE);
+                            return;
+                        }else{
+                            errorMessage.append("Avatar size is too high.");
+                        }
+                        break;
                 }
             }catch (DAOException e){
                 LOGGER.error(e);
+                errorMessage.append(e).append(".");
             }
+        }else{
+            errorMessage.append("No picture was found.");
         }
-        response.sendRedirect(PagePath.USER_MAIN_PAGE);
+        request.setAttribute(AttributeConstant.ERROR_MESSAGE_ATTRIBUTE,errorMessage);
+        request.getRequestDispatcher(PagePath.USER_MAIN_PAGE).forward(request,response);
     }
 }
