cb83ebf01ca82718d42e0d7dcf6c1d11501c9a33
crazyr <alexey98901@gmail.com>
2018-07-17 10:10:31 +0300

    17.07.2018 10:11

--- src/com/epam/creatures/command/SelfDeleteCommand.java
+++ src/com/epam/creatures/command/SelfDeleteCommand.java
@@ -1,6 +1,5 @@
-package com.epam.creatures.command.user;
+package com.epam.creatures.command;
 
-import com.epam.creatures.command.AbstractCommand;
 import com.epam.creatures.constant.AttributeConstant;
 import com.epam.creatures.constant.ParameterConstant;
 import com.epam.creatures.entity.Router;
@@ -23,6 +22,7 @@ public class SelfDeleteCommand extends AbstractCommand {
         HashMap<String,Object> attributeMap = new HashMap<>();
 
         parameterMap.put(ParameterConstant.USER_ID_PARAMETER, String.valueOf(request.getSession().getAttribute(AttributeConstant.ID_ATTRIBUTE)));
+        parameterMap.put(ParameterConstant.ROLE_PARAMETER, String.valueOf(request.getSession().getAttribute(AttributeConstant.ROLE_ATTRIBUTE)));
 
         getService().process(parameterMap,attributeMap);
 
---
+++ src/com/epam/creatures/service/SelfDeleteService.java
@@ -0,0 +1,63 @@
+package com.epam.creatures.service;
+
+import com.epam.creatures.constant.AttributeConstant;
+import com.epam.creatures.constant.PagePath;
+import com.epam.creatures.constant.ParameterConstant;
+import com.epam.creatures.dao.DAOException;
+import com.epam.creatures.dao.impl.AdminDAO;
+import com.epam.creatures.dao.impl.UserDAO;
+import com.epam.creatures.entity.ClientRole;
+import com.epam.creatures.entity.Router;
+import com.epam.creatures.factory.RouterFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Map;
+
+public class SelfDeleteService implements CommandService {
+    private static final String LOG_OUT_PATH = "/creatures?commandType=LOG_OUT_COMMAND";
+    private static final Logger LOGGER = LogManager.getLogger(SelfDeleteService.class);
+    @Override
+    public void process(Map<String, String> parameterMap, Map<String, Object> attributeMap) {
+        RouterFactory routerFactory = new RouterFactory();
+        StringBuilder errorMessage = new StringBuilder();
+        Router.RouteType routeType = Router.RouteType.FORWARD;
+        String route = PagePath.USER_MAIN_PAGE;
+        Integer id = Integer.parseInt(parameterMap.get(ParameterConstant.USER_ID_PARAMETER));
+        ClientRole role = ClientRole.valueOf(parameterMap.get(ParameterConstant.ROLE_PARAMETER));
+
+        try {
+
+            if(role!=null) {
+
+                switch (role) {
+                    case USER:
+                        UserDAO userDAO = new UserDAO();
+                        if (userDAO.delete(id)) {
+                            routeType = Router.RouteType.REDIRECT;
+                            route = LOG_OUT_PATH;
+                        } else {
+                            errorMessage.append("Could not delete user.");
+                        }
+                        break;
+                    case ADMIN:
+                        AdminDAO adminDAO = new AdminDAO();
+                        if (adminDAO.delete(id)) {
+                            routeType = Router.RouteType.REDIRECT;
+                            route = LOG_OUT_PATH;
+                        } else {
+                            errorMessage.append("Could not delete admin.");
+                        }
+                }
+            }else {
+                errorMessage.append("Client is not logged in.");
+            }
+        } catch (DAOException e) {
+            LOGGER.debug(e);
+            errorMessage.append(e).append(".");
+        }
+        attributeMap.put(AttributeConstant.ERROR_MESSAGE_ATTRIBUTE,errorMessage);
+        attributeMap.put(AttributeConstant.ROUTER_ATTRIBUTE,routerFactory
+                .createRouter(routeType,route));
+    }
+}
